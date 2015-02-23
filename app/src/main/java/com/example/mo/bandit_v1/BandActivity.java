45b95946f377e1486af8a5b05789d2509821be06
leurer <s.i.m.o.n.1@gmx.at>
2015-02-23 16:17:43 +0100

    uploadbutton

--- app/src/main/java/com/example/mo/bandit_v1/BandActivity.java
+++ app/src/main/java/com/example/mo/bandit_v1/BandActivity.java
@@ -2,12 +2,17 @@ package com.example.mo.bandit_v1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URISyntaxException;
 
 
 public class BandActivity extends Activity {
@@ -48,7 +53,7 @@ public class BandActivity extends Activity {
         editBandBandButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
+               showFileChooser();
             }
         });
     }
@@ -75,4 +80,48 @@ public class BandActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private static final int FILE_SELECT_CODE = 0;
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("mp3");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d("TAG", "File Uri: " + uri.toString());
+                    // Get the path
+                    String path = null;
+                    try {
+                        path = FileUtils.getPath(this, uri);
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("TAG", "File Path: " + path);
+                    // Get the file instance
+                    // File file = new File(path);
+                    // Initiate the upload
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
+
